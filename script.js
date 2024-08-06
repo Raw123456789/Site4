03b71e2cb1e9d6a361e76fb0248c466d7fdcb388
Raw123456789 <margonapkins@gmail.com>
2024-08-06 13:32:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,16 @@ function showTask(){
 }
 showTask()
 
+fetch('https://reqres.in/api/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'User 1'
+    })
+}).then(res => {
+    return res.json()
+})
+.then(data => console.log(data))
+.catch(error => console.log('ERROR'))
